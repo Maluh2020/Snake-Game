815ddb9c90a52d68c33248330bde92c8aa853fcb
Maluh2020 <maluhmyth@gmail.com>
2021-03-06 17:58:00 -0300

    criar movimentos

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ snake[0] = {
 	y: 8*box
 }
 
+var direction = "rigth";
+
 function criarBG(){
 	context.fillStyle = "lightgray";
 	context.fillRect(0,0,16*box, 16*box);
@@ -19,6 +21,35 @@ function criarCobrinha(){
 		context.fillRect(snake[i].x, snake[i].y, box, box);
 	}
 }
+
+function iniciarGame (){
+	criarBG();
+	criarCobrinha();
+
+	var snakeX = snake[0].x;
+	var snakeY = snake[0].y;
+
+	if(direction == "rigth") snakeX += box;
+	if(direction == "left") snakeX -= box;
+	if(direction == "up") snakeY -= box;
+	if(direction == "down") snakeY += box;
+
+	snake.pop();
+
+	var newHead = {
+		x: snakeX,
+		y: snakeY
+	}
+
+	snake.unshift(newHead);
+
+
+}
+
+
+var game = setInterval(iniciarGame, 100);
+
+
 
 criarBG();
 criarCobrinha();
