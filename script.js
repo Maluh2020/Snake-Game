e958a2fc4ee8158b5de4abc4739e57d96a6556ed
Maluh2020 <maluhmyth@gmail.com>
2021-03-06 17:39:14 -0300

    Criar cobrinha

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var canvas = document.getElementById("snake");
 var context = canvas.getContext("2d");
 var box = 32;
+var snake = []
+snake[0] = {
+	x: 8*box,
+	y: 8*box
+}
 
 function criarBG(){
 	context.fillStyle = "lightgray";
@@ -8,4 +13,12 @@ function criarBG(){
 
 }
 
+function criarCobrinha(){
+	for (var i = 0; i < snake.length; i++){
+		context.fillStyle = "brown";
+		context.fillRect(snake[i].x, snake[i].y, box, box);
+	}
+}
+
 criarBG();
+criarCobrinha();
